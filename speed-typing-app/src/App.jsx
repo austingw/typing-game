dcbc7745e0eec14d4f684c5087e3ed5bc6c384b8
Austin Williams <senortinman@gmail.com>
2022-09-26 11:27:48 -0400

    Basic file setup, UI elements, and styling

---
+++ speed-typing-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Speed Typing!</h1>
+      <textarea />
+      <h4>Time Remaining: </h4>
+      <button>Start</button>
+      <h1>Word Count:</h1>
+    </div>
+  );
+}
+
+export default App;
