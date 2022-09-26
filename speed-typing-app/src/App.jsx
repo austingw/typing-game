1c54e25ba8e9e2f2aae5af60c5b6bfa6f8b130ff
Austin Williams <senortinman@gmail.com>
2022-09-26 11:33:29 -0400

    Add state and handleChange function

--- speed-typing-app/src/App.jsx
+++ speed-typing-app/src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from "react";
 
 function App() {
+  const [typed, setTyped] = useState();
+
+  function handleChange(event) {
+    setTyped(event.target.value);
+    console.log(typed);
+  }
+
   return (
     <div className="App">
       <h1>Speed Typing!</h1>
-      <textarea />
+      <textarea onChange={handleChange} />
       <h4>Time Remaining: </h4>
       <button>Start</button>
       <h1>Word Count:</h1>
