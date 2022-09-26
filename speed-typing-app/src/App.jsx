2ae1678c49bc33a51350fea3f6a88038b1f8eb51
Austin Williams <senortinman@gmail.com>
2022-09-26 12:28:28 -0400

    Add game start state

--- speed-typing-app/src/App.jsx
+++ speed-typing-app/src/App.jsx
@@ -3,10 +3,15 @@ import { useEffect, useState } from "react";
 function App() {
   const [typed, setTyped] = useState("");
   const [timer, setTimer] = useState(5);
+  const [start, setStart] = useState(false);
 
   function handleChange(event) {
     setTyped(event.target.value);
     console.log(typed);
+  }
+
+  function startGame() {
+    setStart(true);
   }
 
   function wordCount(text) {
@@ -16,12 +21,12 @@ function App() {
   }
 
   useEffect(() => {
-    const countdown = setTimeout(() => {
-      setTimer((prevTimer) => prevTimer - 1);
-    }, 1000);
-
-    if (timer === 0) {
-      clearTimeout(countdown);
+    if (start === true) {
+      if (timer !== 0) {
+        setTimeout(() => {
+          setTimer((prevTimer) => prevTimer - 1);
+        }, 1000);
+      }
     }
   }, [timer]);
 
@@ -30,7 +35,7 @@ function App() {
       <h1>Speed Typing!</h1>
       <textarea onChange={handleChange} value={typed} />
       <h4>Time Remaining: {timer} seconds</h4>
-      <button onClick={() => wordCount(typed)}>Start</button>
+      <button onClick={startGame}>Start</button>
       <h1>Word Count:</h1>
     </div>
   );
