a7f56b9039bd33062699163cd71f4d5b75dad050
Austin Williams <senortinman@gmail.com>
2022-09-26 11:52:45 -0400

    Add word count function to start button

--- speed-typing-app/src/App.jsx
+++ speed-typing-app/src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
 
 function App() {
-  const [typed, setTyped] = useState();
+  const [typed, setTyped] = useState("");
 
   function handleChange(event) {
     setTyped(event.target.value);
     console.log(typed);
   }
 
+  function wordCount(text) {
+    const wordsArr = text.split(" ");
+    console.log(wordsArr.length);
+  }
+
   return (
     <div className="App">
       <h1>Speed Typing!</h1>
-      <textarea onChange={handleChange} />
+      <textarea onChange={handleChange} value={typed} />
       <h4>Time Remaining: </h4>
-      <button>Start</button>
+      <button onClick={() => wordCount(typed)}>Start</button>
       <h1>Word Count:</h1>
     </div>
   );
