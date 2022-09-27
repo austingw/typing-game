4e9e50c040fdae9d847449a2da4dd90bc07bcfb9
Austin Williams <senortinman@gmail.com>
2022-09-27 10:55:00 -0400

    Move game logic and state to custom hook that imports to App

--- speed-typing-app/src/App.jsx
+++ speed-typing-app/src/App.jsx
@@ -1,46 +1,15 @@
-import { useEffect, useState, useRef } from "react";
+import useGame from "./useGame";
 
 function App() {
-  const STARTING_TIME = 15;
-  const textboxRef = useRef(null);
-
-  const [typed, setTyped] = useState("");
-  const [timer, setTimer] = useState(STARTING_TIME);
-  const [start, setStart] = useState(false);
-  const [wordCount, setWordCount] = useState(0);
-
-  function handleChange(event) {
-    setTyped(event.target.value);
-  }
-
-  function startGame() {
-    setStart(true);
-    setTyped("");
-    setTimer(STARTING_TIME);
-    setWordCount(0);
-    textboxRef.current.disabled = false;
-    textboxRef.current.focus();
-  }
-
-  function endGame() {
-    setStart(false);
-    setWordCount(calculateWordCount(typed));
-  }
-
-  function calculateWordCount(text) {
-    const wordsArr = text.trim().split(" ");
-    return wordsArr.filter((word) => word !== "").length;
-  }
-
-  useEffect(() => {
-    if (start && timer !== 0) {
-      setTimeout(() => {
-        setTimer((prevTimer) => prevTimer - 1);
-      }, 1000);
-    } else {
-      endGame();
-    }
-  }, [timer, start]);
+  const {
+    textboxRef,
+    handleChange,
+    typed,
+    start,
+    timer,
+    startGame,
+    wordCount,
+  } = useGame();
 
   return (
     <div className="App">
